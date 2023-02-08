783784c4be2321726fdc003f3baf64e44f873050
Your Name <you@example.com>
2023-02-08 13:54:21 +0530

    Added: Create employee payroll to read write to a console

--- src/main/java/com/bridgelabz/EmployeePayroll.java
+++
@@ -1,9 +0,0 @@
-package com.bridgelabz;
-
-public class EmployeePayroll {
-
-    public static void main(String[] args) {
-
-        System.out.println("...Welcome To Employee Payroll Service JAVA IO Program...");
-    }
-}
